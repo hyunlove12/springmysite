02dae2805601a26f990b2e8970a24202f2be6986
donghyun <hyunlove12@naver.com>
2019-12-27 14:23:06 +0900

    브런치 테스트

--- src/main/java/com/bigdata2019/mysite/controller/MainController.java
+++ src/main/java/com/bigdata2019/mysite/controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	
+	//브런치 테스트
 	@RequestMapping({"/", "/index", "/main"})	
 	public String main() {
 		return "main/index";
