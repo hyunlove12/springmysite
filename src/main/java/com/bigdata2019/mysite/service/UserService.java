2346db1bcfa80e6d673b91469b5ab00b95343a04
donghyun <hyunlove12@naver.com>
2019-12-27 14:20:25 +0900

    수정

--- src/main/java/com/bigdata2019/mysite/service/UserService.java
+++ src/main/java/com/bigdata2019/mysite/service/UserService.java
@@ -2,6 +2,7 @@ package com.bigdata2019.mysite.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.handler.UserRoleAuthorizationInterceptor;
 
 import com.bigdata2019.mysite.repository.UserDao;
 
@@ -17,6 +18,11 @@ public class UserService {
 	
 	public UserVo getUser(String email, String password) {
 		UserVo userVo = userRepository.find(email, password);
+		return userVo;
+	}
+
+	public UserVo getUser(Long no) {
+		UserVo userVo = (UserVo)userRepository.find(no);		
 		return userVo;
 	}	
 }
