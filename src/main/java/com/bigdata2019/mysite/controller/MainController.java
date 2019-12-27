70e6a71e290eb3473822b9e96edbe8a6aaf3940e
donghyun <hyunlove12@naver.com>
2019-12-27 17:24:06 +0900

    git 마스터

--- src/main/java/com/bigdata2019/mysite/controller/MainController.java
+++ src/main/java/com/bigdata2019/mysite/controller/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-	
+	//master
 	@RequestMapping({"/", "/index", "/main"})	
 	public String main() {
 		return "main/index";
